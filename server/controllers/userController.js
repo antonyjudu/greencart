61105fff13bb54def3d2a0e8056a3c0f340a2d5e
JUDU <antonyjudu18@gmail.com>
2025-08-26 21:33:40 +0530

    Project Completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,7 +21,7 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,  // Prevent JavaScript to access cookie
             secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? none :'strict', // CSRF Production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' :'strict', // CSRF Production
             maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time
         })
         return res.json({success: true, user: {email: user.email, name: user.name}});
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,  
             secure: process.env.NODE_ENV === 'production', 
-            sameSite: process.env.NODE_ENV === 'production' ? none :'strict', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' :'strict', 
             maxAge: 7 * 24 * 60 * 60 * 1000, 
         });
         return res.json({success: true, user: {email: user.email, name: user.name}});
