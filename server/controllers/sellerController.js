dbd6c965e57826a40a863b4cf365aa0625dc544b
JUDU <antonyjudu18@gmail.com>
2025-08-28 12:09:09 +0530

    Project Completed

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -9,7 +9,7 @@ export const sellerLogin = async (req, res) => {
             res.cookie('sellerToken', token, {
                 httpOnly: true,  
                 secure: process.env.NODE_ENV === 'production', 
-                sameSite: process.env.NODE_ENV === 'production' ? none :'strict', 
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' :'strict', 
                 maxAge: 7 * 24 * 60 * 60 * 1000, 
             });
             return res.json({success: true, message: "Logged In!"});
