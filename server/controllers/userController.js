2e2ccf34ac747ffd01684d1e541ff89a6d1ae387
JUDU <antonyjudu18@gmail.com>
2025-09-04 14:17:59 +0530

    commit added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -82,7 +82,8 @@ export  const logout = async (req, res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            path: '/' 
         });
         return res.json({success: true, message: 'Logged Out!'});
     } catch (error) {
