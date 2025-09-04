799a166fd049a7b17c3013529985b8c48198ae2c
JUDU <antonyjudu18@gmail.com>
2025-09-04 22:16:44 +0530

    First Commit

--- server/server.js
+++ server/server.js
@@ -26,7 +26,16 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 // Middleware Configuration
 app.use(express.json()); 
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin); // dynamically set origin
+    }
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.get('/', (req, res) => res.send('API is working!'));
